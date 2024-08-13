a5158e7939c6a88d4288c981afcc3b1ceaf1aa25
WilliamThimm <William.Thimm@gmail.com>
2024-08-13 16:37:01 +0200

    Added BranchCache and TagCache

---
+++ src/main/java/de/kontext_e/jqassistant/plugin/git/scanner/cache/BranchCache.java
@@ -0,0 +1,45 @@
+package de.kontext_e.jqassistant.plugin.git.scanner.cache;
+
+import com.buschmais.jqassistant.core.store.api.Store;
+import de.kontext_e.jqassistant.plugin.git.scanner.model.GitBranch;
+import de.kontext_e.jqassistant.plugin.git.store.descriptor.GitBranchDescriptor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Map;
+
+import static de.kontext_e.jqassistant.plugin.git.scanner.JQAssistantGitRepository.importExistingBranchesFromStore;
+
+public class BranchCache {
+
+    private final Logger LOGGER = LoggerFactory.getLogger(BranchCache.class);
+    private final Map<String, GitBranchDescriptor> branches;
+    private final Store store;
+
+    public BranchCache(Store store) {
+        this.store = store;
+        this.branches = importExistingBranchesFromStore(store);
+    }
+
+    public GitBranchDescriptor findOrCreate(GitBranch gitBranch) {
+        String name = gitBranch.getName().replaceFirst("refs/", "");
+
+        if (branches.containsKey(name)){
+            return branches.get(name);
+        }
+
+        return createBranchDescriptor(gitBranch, name);
+    }
+
+    private GitBranchDescriptor createBranchDescriptor(GitBranch gitBranch, String name) {
+        LOGGER.debug ("Adding new Branch '{}' with Head '{}'", name, gitBranch.getCommitSha());
+        GitBranchDescriptor gitBranchDescriptor = store.create(GitBranchDescriptor.class);
+
+        String sha = gitBranch.getCommitSha();
+        gitBranchDescriptor.setName(name);
+
+        branches.put(name, gitBranchDescriptor);
+        return gitBranchDescriptor;
+    }
+
+}
---
+++ src/main/java/de/kontext_e/jqassistant/plugin/git/scanner/cache/TagCache.java
@@ -0,0 +1,44 @@
+package de.kontext_e.jqassistant.plugin.git.scanner.cache;
+
+import com.buschmais.jqassistant.core.store.api.Store;
+import de.kontext_e.jqassistant.plugin.git.scanner.model.GitTag;
+import de.kontext_e.jqassistant.plugin.git.store.descriptor.GitTagDescriptor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Map;
+
+import static de.kontext_e.jqassistant.plugin.git.scanner.JQAssistantGitRepository.importExistingTagsFromStore;
+
+public class TagCache {
+
+    private final Logger LOGGER = LoggerFactory.getLogger(TagCache.class);
+    private final Map<String, GitTagDescriptor> tags;
+    private final Store store;
+
+    public TagCache(Store store) {
+        this.store = store;
+        this.tags = importExistingTagsFromStore(store);
+    }
+
+    public GitTagDescriptor findOrCreate(GitTag gitTag){
+        String tagLabel = gitTag.getLabel().replaceFirst("refs/tags/", "");
+
+        if (tags.containsKey(tagLabel)) {
+            return tags.get(tagLabel);
+        }
+
+        return createTagDescriptor(tagLabel);
+    }
+
+    private GitTagDescriptor createTagDescriptor(String label) {
+        LOGGER.debug("Adding new Tag '{}'", label);
+        GitTagDescriptor gitTagDescriptor = store.create(GitTagDescriptor.class);
+
+        gitTagDescriptor.setLabel(label);
+
+        tags.put(label, gitTagDescriptor);
+        return gitTagDescriptor;
+    }
+
+}
