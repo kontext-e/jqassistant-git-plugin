434eb1d24acf12bf3f95ee2eb465d763269a02d7
WilliamThimm <William.Thimm@gmail.com>
2024-08-13 14:56:36 +0200

    Extracted FileAnalyzer from GitRepositoryScanner and added FileCache

--- src/main/java/de/kontext_e/jqassistant/plugin/git/scanner/GitRepositoryScanner.java
+++ src/main/java/de/kontext_e/jqassistant/plugin/git/scanner/GitRepositoryScanner.java
@@ -4,26 +4,18 @@ import com.buschmais.jqassistant.core.store.api.Store;
 import de.kontext_e.jqassistant.plugin.git.scanner.cache.AuthorCache;
 import de.kontext_e.jqassistant.plugin.git.scanner.cache.CommitCache;
 import de.kontext_e.jqassistant.plugin.git.scanner.cache.CommitterCache;
+import de.kontext_e.jqassistant.plugin.git.scanner.cache.FileCache;
 import de.kontext_e.jqassistant.plugin.git.scanner.model.GitBranch;
 import de.kontext_e.jqassistant.plugin.git.scanner.model.GitChange;
 import de.kontext_e.jqassistant.plugin.git.scanner.model.GitCommit;
 import de.kontext_e.jqassistant.plugin.git.scanner.model.GitTag;
-import de.kontext_e.jqassistant.plugin.git.store.descriptor.GitAuthorDescriptor;
-import de.kontext_e.jqassistant.plugin.git.store.descriptor.GitBranchDescriptor;
-import de.kontext_e.jqassistant.plugin.git.store.descriptor.GitChangeDescriptor;
-import de.kontext_e.jqassistant.plugin.git.store.descriptor.GitCommitDescriptor;
-import de.kontext_e.jqassistant.plugin.git.store.descriptor.GitCommitterDescriptor;
-import de.kontext_e.jqassistant.plugin.git.store.descriptor.GitFileDescriptor;
-import de.kontext_e.jqassistant.plugin.git.store.descriptor.GitRepositoryDescriptor;
-import de.kontext_e.jqassistant.plugin.git.store.descriptor.GitTagDescriptor;
+import de.kontext_e.jqassistant.plugin.git.store.descriptor.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -34,15 +26,16 @@ public class GitRepositoryScanner {
 
     public static final DateFormat DATE_FORMAT = new SimpleDateFormat("yyyy-MM-dd");
     public static final DateFormat TIME_FORMAT = new SimpleDateFormat("HH:mm:ss Z");
-    private static final DateFormat DATE_TIME_FORMAT = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss Z");
+    public static final DateFormat DATE_TIME_FORMAT = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss Z");
 
     private final Store store;
     private final GitRepositoryDescriptor gitRepositoryDescriptor;
     private final CommitCache commitCache;
     private final AuthorCache authorCache;
     private final CommitterCache committerCache;
+    private final FileAnalyzer fileAnalyzer;
+    private final FileCache fileCache;
     private String range;
-    private final Map<String, GitFileDescriptor> files = new HashMap<>();
     private final Map<String, GitBranchDescriptor> branches;
     private final Map<String, GitTagDescriptor> tags;
 
@@ -54,6 +47,9 @@ public class GitRepositoryScanner {
         this.commitCache = new CommitCache(store);
         this.authorCache = new AuthorCache(store);
         this.committerCache = new CommitterCache(store);
+        this.fileCache = new FileCache(store);
+
+        this.fileAnalyzer = new FileAnalyzer(fileCache);
 
         this.branches = importExistingBranchesFromStore(store);
         this.tags = importExistingTagsFromStore(store);
@@ -78,8 +74,7 @@ public class GitRepositoryScanner {
 
         authorCache.getAuthors().forEach(gitAuthor -> gitRepositoryDescriptor.getAuthors().add(gitAuthor));
         committerCache.getCommiters().forEach(gitCommitter -> gitRepositoryDescriptor.getCommitters().add(gitCommitter));
-        //TODO Avoid duplicate scanning
-        files.values().forEach(gitFile -> gitRepositoryDescriptor.getFiles().add(gitFile));
+        fileCache.getFiles().forEach(gitFile -> gitRepositoryDescriptor.getFiles().add(gitFile));
 
         GitBranch head = jGitRepository.findHead();
         GitCommitDescriptor headDescriptor = commitCache.get(head.getCommitSha());
@@ -99,6 +94,15 @@ public class GitRepositoryScanner {
             addCommitForAuthor(gitCommit.getAuthor(), descriptor);
             addCommitForCommitter(gitCommit.getCommitter(), descriptor);
             addCommitFiles(gitCommit, descriptor);
+        }
+    }
+
+    void addCommitFiles(final GitCommit gitCommit, final GitCommitDescriptor gitCommitDescriptor) {
+        for (GitChange gitChange : gitCommit.getGitChanges()) {
+            GitChangeDescriptor gitChangeDescriptor = store.create(GitChangeDescriptor.class);
+            gitChangeDescriptor.setModificationKind(gitChange.getModificationKind());
+            gitCommitDescriptor.getChanges().add(gitChangeDescriptor);
+            fileAnalyzer.addAsGitFile(gitChange, gitChangeDescriptor, gitCommit.getDate());
         }
     }
 
@@ -185,80 +189,6 @@ public class GitRepositoryScanner {
 
         GitCommitterDescriptor committerDescriptor = committerCache.findOrCreate(committer);
         committerDescriptor.getCommits().add(gitCommit);
-    }
-
-    private void addCommitFiles(final GitCommit gitCommit, final GitCommitDescriptor gitCommitDescriptor) {
-        for (GitChange gitChange : gitCommit.getGitChanges()) {
-            GitChangeDescriptor gitChangeDescriptor = store.create(GitChangeDescriptor.class);
-            gitChangeDescriptor.setModificationKind(gitChange.getModificationKind());
-            gitCommitDescriptor.getChanges().add(gitChangeDescriptor);
-            addAsGitFile(gitChange, gitChangeDescriptor, gitCommit.getDate());
-        }
-    }
-
-    private void addAsGitFile(GitChange gitChange, final GitChangeDescriptor gitChangeDescriptor, final Date date) {
-        final GitFileDescriptor gitFileDescriptor = getOrCreateGitFileDescriptor(gitChange.getRelativePath());
-
-        gitChangeDescriptor.setModifies(gitFileDescriptor);
-
-        if(isAddChange(gitChangeDescriptor)) {
-            gitFileDescriptor.setCreatedAt(DATE_TIME_FORMAT.format(date));
-            gitFileDescriptor.setCreatedAtEpoch(date.getTime());
-            gitChangeDescriptor.setCreates(gitFileDescriptor);
-        } else if(isUpdateChange(gitChangeDescriptor)) {
-            gitChangeDescriptor.setUpdates(gitFileDescriptor);
-            if (gitFileDescriptor.getLastModificationAtEpoch() == null || date.getTime() < gitFileDescriptor.getLastModificationAtEpoch()){
-                gitFileDescriptor.setLastModificationAt(DATE_TIME_FORMAT.format(date));
-                gitFileDescriptor.setLastModificationAtEpoch(date.getTime());
-            }
-        } else if(isDeleteChange(gitChangeDescriptor)) {
-            gitFileDescriptor.setDeletedAt(DATE_TIME_FORMAT.format(date));
-            gitFileDescriptor.setDeletedAtEpoch(date.getTime());
-            gitChangeDescriptor.setDeletes(gitFileDescriptor);
-        } else if(isRenameChange(gitChangeDescriptor)) {
-            final GitFileDescriptor oldFile = getOrCreateGitFileDescriptor(gitChange.getOldPath());
-            final GitFileDescriptor newFile = getOrCreateGitFileDescriptor(gitChange.getNewPath());
-            oldFile.setHasNewName(newFile);
-            gitChangeDescriptor.setRenames(oldFile);
-            gitChangeDescriptor.setDeletes(oldFile);
-            gitChangeDescriptor.setCreates(newFile);
-        } else if(isCopyChange(gitChangeDescriptor)) {
-            final GitFileDescriptor oldFile = getOrCreateGitFileDescriptor(gitChange.getOldPath());
-            final GitFileDescriptor newFile = getOrCreateGitFileDescriptor(gitChange.getNewPath());
-            newFile.setCopyOf(oldFile);
-            gitChangeDescriptor.setCopies(oldFile);
-            gitChangeDescriptor.setCreates(newFile);
-        }
-    }
-
-    private boolean isCopyChange(final GitChangeDescriptor gitChangeDescriptor) {
-        return "C".equalsIgnoreCase(gitChangeDescriptor.getModificationKind());
-    }
-
-    private boolean isRenameChange(final GitChangeDescriptor gitChangeDescriptor) {
-        return "R".equalsIgnoreCase(gitChangeDescriptor.getModificationKind());
-    }
-
-    private boolean isDeleteChange(final GitChangeDescriptor gitChangeDescriptor) {
-        return "D".equalsIgnoreCase(gitChangeDescriptor.getModificationKind());
-    }
-
-    private boolean isUpdateChange(final GitChangeDescriptor gitChangeDescriptor) {
-        return "M".equalsIgnoreCase(gitChangeDescriptor.getModificationKind());
-    }
-
-    private boolean isAddChange(final GitChangeDescriptor gitChangeDescriptor) {
-        return "A".equalsIgnoreCase(gitChangeDescriptor.getModificationKind());
-    }
-
-    private GitFileDescriptor getOrCreateGitFileDescriptor(String relativePath) {
-        GitFileDescriptor gitFileDescriptor = files.get(relativePath);
-        if(gitFileDescriptor == null) {
-            gitFileDescriptor = store.create(GitFileDescriptor.class);
-            gitFileDescriptor.setRelativePath(relativePath);
-            files.put(relativePath, gitFileDescriptor);
-        }
-        return gitFileDescriptor;
     }
 
 }
