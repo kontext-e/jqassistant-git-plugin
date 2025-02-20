baf963073d03a7911bceac317ed17af652c3de34
WilliamThimm <William.Thimm@gmail.com>
2025-02-20 15:25:59 +0100

    Added Deterministic behavior for overriding create, delete and modify dates

--- src/main/java/de/kontext_e/jqassistant/plugin/git/store/descriptor/GitFileDescriptor.java
+++ src/main/java/de/kontext_e/jqassistant/plugin/git/store/descriptor/GitFileDescriptor.java
@@ -3,6 +3,9 @@ package de.kontext_e.jqassistant.plugin.git.store.descriptor;
 import com.buschmais.xo.neo4j.api.annotation.Label;
 import com.buschmais.xo.neo4j.api.annotation.Property;
 import com.buschmais.xo.neo4j.api.annotation.Relation;
+import de.kontext_e.jqassistant.plugin.git.scanner.GitRepositoryScanner;
+
+import java.util.Date;
 
 @Label("File")
 public interface GitFileDescriptor extends GitDescriptor {
@@ -42,4 +45,28 @@ public interface GitFileDescriptor extends GitDescriptor {
     @Relation("COPY_OF")
     GitFileDescriptor getCopyOf();
     void setCopyOf(GitFileDescriptor oldFile);
+
+    default void updateDeletionTime(Date date) {
+        //Always take latest delete Change
+        if (getDeletedAtEpoch() == null || date.getTime() < getDeletedAtEpoch()) {
+            setDeletedAt(GitRepositoryScanner.DATE_TIME_FORMAT.format(date));
+            setDeletedAtEpoch(date.getTime());
+        }
+    }
+
+    default void updateCreationTime(Date date) {
+        //Always take earliest create change
+        if (getCreatedAt() == null || date.getTime() > getCreatedAtEpoch()) {
+            setCreatedAt(GitRepositoryScanner.DATE_TIME_FORMAT.format(date));
+            setCreatedAtEpoch(date.getTime());
+        }
+    }
+
+    default void updateLastModificationTime(Date date) {
+        //Always take latest update change
+        if (getLastModificationAtEpoch() == null || date.getTime() < getLastModificationAtEpoch()) {
+            setLastModificationAt(GitRepositoryScanner.DATE_TIME_FORMAT.format(date));
+            setLastModificationAtEpoch(date.getTime());
+        }
+    }
 }
