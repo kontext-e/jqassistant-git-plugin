d13f255b6c2968c97ffa27ea6791faa59f0a9e58
WilliamThimm <William.Thimm@gmail.com>
2025-02-24 15:27:30 +0100

    created new Package for change descriptors

--- src/main/java/de/kontext_e/jqassistant/plugin/git/store/descriptor/GitCommitDescriptor.java
+++ src/main/java/de/kontext_e/jqassistant/plugin/git/store/descriptor/GitCommitDescriptor.java
@@ -3,6 +3,7 @@ package de.kontext_e.jqassistant.plugin.git.store.descriptor;
 import com.buschmais.xo.neo4j.api.annotation.Label;
 import com.buschmais.xo.neo4j.api.annotation.Property;
 import com.buschmais.xo.neo4j.api.annotation.Relation;
+import de.kontext_e.jqassistant.plugin.git.store.descriptor.change.GitChangeDescriptor;
 
 import java.util.List;
 
--- src/main/java/de/kontext_e/jqassistant/plugin/git/store/descriptor/change/GitCopyChangeDescriptor.java
+++ src/main/java/de/kontext_e/jqassistant/plugin/git/store/descriptor/change/GitCopyChangeDescriptor.java
@@ -1,7 +1,8 @@
-package de.kontext_e.jqassistant.plugin.git.store.descriptor;
+package de.kontext_e.jqassistant.plugin.git.store.descriptor.change;
 
 import com.buschmais.xo.neo4j.api.annotation.Label;
 import com.buschmais.xo.neo4j.api.annotation.Relation;
+import de.kontext_e.jqassistant.plugin.git.store.descriptor.GitFileDescriptor;
 
 @Label("Copy")
 public interface GitCopyChangeDescriptor extends GitChangeDescriptor {
